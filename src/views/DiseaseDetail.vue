1041b440d6f0ef93c787eaf7154d99564e8a717e
Mr.Psy <1294640713@qq.com>
2024-09-18 19:19:26 +0800

    Add more disease discription in backend, modified the DiseaseDetail.vue in frontend

--- src/views/DiseaseDetail.vue
+++ src/views/DiseaseDetail.vue
@@ -5,7 +5,13 @@
       <el-tab-pane label="概述" name="overview">{{ disease.overview }}</el-tab-pane>
       <el-tab-pane label="症状" name="symptoms">{{ disease.symptoms.join(', ') }}</el-tab-pane>
       <el-tab-pane label="病因" name="causes">{{ disease.causes.join(', ') }}</el-tab-pane>
-      <!-- 其他标签同理 -->
+      <el-tab-pane label="检查方法" name="tests">{{disease.tests.join(', ')}}</el-tab-pane>
+      <el-tab-pane label="治疗" name="treatments">{{disease.treatments.join(', ')}}</el-tab-pane>
+      <el-tab-pane label="药物" name="medications">{{disease.medications.join(', ')}}</el-tab-pane>
+      <el-tab-pane label="预后" name="prognosis">{{disease.prognosis}}</el-tab-pane> <!-- 无需join -->
+      <el-tab-pane label="护理" name="care">{{disease.care}}</el-tab-pane> <!-- 无需join -->
+      <el-tab-pane label="预防措施" name="prevention">{{disease.prevention}}</el-tab-pane> <!-- 无需join -->
+      <el-tab-pane label="传播途径" name="transmission">{{disease.transmission}}</el-tab-pane> <!-- 无需join -->
     </el-tabs>
   </div>
 </template>
